2a5f18760c8541173992a844b9d95a96b0e2a381
EdsonA8911 <edson.azurduy@uab.com.bo>
2025-09-22 18:17:43 -0400

    feat(router+a11y): move hero to Home and add proper page containers

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,17 @@
 import { Routes } from '@angular/router';
 import { Layout } from './layout/layout';
-import { HomeComponent } from './pages/home/home';   // <— ruta y nombre correctos
+import { HomeComponent } from './pages/home/home';   
 import { About } from './about/about';
+import { UiComponent } from './pages/ui/ui';
 
 export const routes: Routes = [
   {
     path: '',
     component: Layout,
     children: [
-      { path: '', component: HomeComponent, title: 'Inicio' },  // <— usa HomeComponent
+      { path: '', component: HomeComponent, title: 'Inicio' },  
       { path: 'about', component: About, title: 'Acerca de' },
+      { path: 'ui', component: UiComponent, title: 'UI · Demo' },
     ],
   },
 ];
--- src/app/layout/layout.ts
+++ src/app/layout/layout.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './layout.html',
-  styleUrl: './layout.scss'
+  styleUrls: ['./layout.scss'],
 })
-export class Layout {
-
-}
+export class Layout {}
